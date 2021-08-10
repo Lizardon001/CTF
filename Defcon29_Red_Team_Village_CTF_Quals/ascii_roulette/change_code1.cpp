f005c799f47684c9c2c5c1abbba39d047a97e3cc
Lizardon001 <76566693+Lizardon001@users.noreply.github.com>
2021-08-10 10:07:58 +0500

    Create change_code1.cpp

---
+++ Defcon29_Red_Team_Village_CTF_Quals/ascii_roulette/change_code1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+        while(1==1){
+        string flag;
+        cout <<"Enter Any string to Make it string: ";
+        cin >> flag;
+        srand(time(NULL));
+        int flag_rand = (rand()%7)+1;
+        cout << "flag_rand:"<< flag_rand << " ";
+        cout << "Your string Numbers are: ";
+        for(int i = 0; i < flag.length(); i++)
+        {
+                int flag_num = flag[i];
+                flag_num = flag_num ^ flag_rand;
+                if(i % 2 == 0)
+                {
+                        flag_num = flag_num + 24;
+                }
+                else
+                {
+                        flag_num = flag_num - 24;
+                }
+                cout <<flag_num <<" ";
+        }}
+}
